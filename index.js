8952ad3ef547e37339065532768e62940b960409
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-07-21 13:56:22 -0500

    Read json data and serve it. Let curl post to file

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
+const fs = require("fs");
 const express = require("express");
+
+let notes_data = {};
+fs.readFile("./notes.json", "utf8", (err, data) => {
+  if (err) throw err;
+  notes_data = JSON.parse(data).notes;
+  console.log(notes_data);
+});
 
 const app = express();
 app.use(express.json());
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello</h1>`);
 });
 
 app.get("/api/notes", (req, res) => {
-  res.json(notes);
+  res.json(notes_data);
 });
 
 app.get("/api/notes/:id", (req, res) => {
   const target_note_id = Number(req.params.id);
-  const target_note = notes.find((note) => note.id === target_note_id);
+  const target_note = notes_data.find((note) => note.id === target_note_id);
 
   if (target_note) {
     res.json(target_note);
@@ -45,14 +32,29 @@ app.get("/api/notes/:id", (req, res) => {
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-  notes = notes.filter((note) => note.id !== Number(req.params.id));
+  notes_data = notes_data.filter((note) => note.id !== Number(req.params.id));
   res.status(204).end();
 });
 
+const generate_id = () => {
+  return notes_data.length > 0
+    ? Math.max(notes_data.map((note) => note.id)) + 1
+    : 0;
+};
+
 app.post("/api/notes", (req, res) => {
-  const note = req.body;
-  console.log(note);
-  res.json(note);
+  const req_body = req.body;
+  if (!req_body.content) {
+    return res.status(400).json({ error: "Content not found" });
+  }
+  const new_note = {
+    content: req_body.content,
+    important: req_body.important || false,
+    date: new Date(),
+    id: generate_id(),
+  };
+  notes_data = notes_data.concat(new_note);
+  res.json(new_note);
 });
 
 const port = 9001;
