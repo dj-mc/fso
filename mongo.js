cd1bb2cc5780de96a5796db190afc8cb5e73edd0
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-08-19 14:57:33 -0500

    Extract models from cli program

--- mongo.js
+++ mongo.js
@@ -1,62 +1,31 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const { Note, save_new_note, Contact, save_new_contact } = require('./models');
 
-// node mongo.js <password> notes <data>
-// node mongo.js <password> phonebook <data>
+// CLI to mongodb
+// node mongo.js notes <data>
+// node mongo.js phonebook <data>
 
-if (process.argv.length < 4) {
-  console.log('Missing arguments? Try: node mongo.js <password> <db> <data>');
+if (process.argv.length < 3) {
+  console.log('Missing arguments? Try: node mongo.js <db> <data>');
   process.exit(1);
 }
 
-const password = process.argv[2];
-const mongodb_url = `mongodb+srv://dj-mc:${password}@cluster0.at72vrm.mongodb.net/?retryWrites=true&w=majority`;
-
-const db = process.argv[3]; // Expecting notes or phonebook
+const mongodb_uri = process.env.MONGODB_URI;
+const db = process.argv[2]; // Expecting notes or phonebook
 // Expecting a quoted JSON string eg '{"data":42, "error":true}'
-const json_data = JSON.parse(process.argv[4]);
-
-const note_schema = new mongoose.Schema({
-  content: { type: String },
-  date: { type: Date },
-  important: { type: Boolean }
-});
-const Note = mongoose.model('Note', note_schema);
-
-const contact_schema = new mongoose.Schema({
-  name: { type: String },
-  phone_number: { type: String },
-  id: { type: Number }
-});
-const Contact = mongoose.model('Contact', contact_schema);
-
-const save_new_note = () => {
-  const new_date = new Date(Date.now());
-  const new_note = new Note({
-    ...json_data,
-    date: json_data.date || new_date.toISOString(),
-    important: json_data.important || false
-  });
-  return new_note.save();
-};
-
-const save_new_contact = () => {
-  const new_contact = new Contact({
-    ...json_data,
-    id: json_data.id || Math.random() * 10e16
-  });
-  return new_contact.save();
-};
+const json_data = JSON.parse(process.argv[3]);
 
 mongoose
-  .connect(mongodb_url)
+  .connect(mongodb_uri)
 
-  .then((result) => {
-    console.log(`Connected to mongoose. Result: ${result}`);
+  .then((_) => {
+    console.log('Connected to mongoose.');
     switch (db) {
       case 'notes':
-        return save_new_note();
+        return save_new_note(json_data);
       case 'phonebook':
-        return save_new_contact();
+        return save_new_contact(json_data);
       default:
         return () => {
           console.log(`Couldn't find ${db}`);
