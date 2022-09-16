f47f01b0a2dcb269e4f565935493087e6bcce9f3
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-09-16 11:59:27 -0500

    Split models

---
+++ models/Contact.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongodb_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongodb_URI)
+  .then((_) => {
+    console.log(`Connected to ${mongodb_URI}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+const contact_schema = new mongoose.Schema({
+  name: { type: String },
+  phone_number: { type: String },
+  id: { type: Number }
+});
+
+contact_schema.set('toJSON', {
+  transform: (document, returned_obj) => {
+    returned_obj.id = returned_obj._id.toString();
+    delete returned_obj._id;
+    delete returned_obj.__v;
+  }
+});
+
+contact_schema.set('toObject', {
+  transform: (document, returned_obj) => {
+    returned_obj.id = returned_obj._id.toString();
+    delete returned_obj._id;
+    delete returned_obj.__v;
+  }
+});
+
+// const Contact = mongoose.model('Contact', contact_schema);
+
+// const new_contact = (req_body) => {
+//   return new Contact({
+//     name: req_body.name,
+//     phone_number: req_body.phone_number
+//   });
+// };
+
+// module.exports = { Contact, new_contact };
+module.exports = mongoose.model('Contact', contact_schema);
---
+++ models/Note.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongodb_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(mongodb_URI)
+  .then((_) => {
+    console.log(`Connected to ${mongodb_URI}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+const note_schema = new mongoose.Schema({
+  content: { type: String },
+  date: { type: Date },
+  important: { type: Boolean }
+});
+
+note_schema.set('toJSON', {
+  transform: (document, returned_obj) => {
+    returned_obj.id = returned_obj._id.toString();
+    delete returned_obj._id;
+    delete returned_obj.__v;
+  }
+});
+
+note_schema.set('toObject', {
+  transform: (document, returned_obj) => {
+    returned_obj.id = returned_obj._id.toString();
+    delete returned_obj._id;
+    delete returned_obj.__v;
+  }
+});
+
+// const Note = mongoose.model('Note', note_schema);
+
+// const new_note = (req_body) => {
+//   return new Note({
+//     content: req_body.content,
+//     date: new Date(),
+//     important: req_body.important || false
+//   });
+// };
+
+// module.exports = { Note, new_note };
+module.exports = mongoose.model('Note', note_schema);
