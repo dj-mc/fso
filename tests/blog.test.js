06c2aaa9dd06bbfc87a147dc910a0d4aecf47a66
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-09-28 22:17:13 -0500

    Add tests for parsing blog data

---
+++ tests/blog.test.js
@@ -0,0 +1,37 @@
+const { describe, expect, test } = require('@jest/globals');
+const { total_likes, favorite_blog } = require('../utils/list-helper');
+const full_blog_list = require('../test-data/blog.json');
+
+describe('Total sum of likes in a blog list', () => {
+  const one_article_blog_list = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ];
+
+  test('equals the "likes" property data of its one article', () => {
+    const result = total_likes(one_article_blog_list);
+    expect(result).toBe(5);
+  });
+
+  test('equals the total number of likes in each listing', () => {
+    const result = total_likes(full_blog_list.blogs);
+    expect(result).toBe(36);
+  });
+});
+
+test('Return the top blog listing according to likes', () => {
+  const result = favorite_blog(full_blog_list.blogs);
+  expect(result).toEqual({
+    // Exclude _id and __v
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  });
+});
---
+++ utils/list-helper.js
@@ -0,0 +1,24 @@
+const total_likes = (blog_list_array) => {
+  if (blog_list_array.length === 0) return 0;
+  return blog_list_array.reduce((sum, current_listing) => {
+    return sum + current_listing.likes;
+  }, 0);
+};
+
+const favorite_blog = (blog_list_array) => {
+  const likes_values = [];
+  for (const blog of blog_list_array) {
+    likes_values.push(blog.likes);
+  }
+
+  const idx = likes_values.indexOf(Math.max(...likes_values));
+  const favorite_blog = blog_list_array[idx];
+  delete favorite_blog._id;
+  delete favorite_blog.__v;
+
+  return {
+    ...favorite_blog
+  };
+};
+
+module.exports = { total_likes, favorite_blog };
