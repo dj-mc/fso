9dea9f27293129471029cea3d30ec1f746ea81fd
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-10-23 01:52:08 -0500

    Test invalid user creation

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const {
   afterAll,
-  beforeEach,
+  beforeAll,
   describe,
   expect,
   test
@@ -15,10 +15,14 @@ const app = require('../app');
 const api = supertest(app);
 
 describe('If there is one user in database', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     await User.deleteMany({});
     const password_hash = await bcrypt.hash('secret$PW%s@lt3d', 10);
-    const new_user = new User({ username: 'root', password_hash });
+    const new_user = new User({
+      username: 'root',
+      name: 'mega admin superuser extreme version 2.0',
+      password_hash // Model takes hash of pw, not its plaintext
+    });
     await new_user.save();
   });
 
@@ -28,7 +32,7 @@ describe('If there is one user in database', () => {
     const new_user = {
       username: 'jsmith',
       name: 'Jane Smith',
-      password: 'foobar123'
+      password: 'foobar123' // API takes plaintext pw
     };
 
     await api
@@ -47,7 +51,7 @@ describe('If there is one user in database', () => {
   test('fail to create a new user whose username is already taken', async () => {
     const all_users = await get_all_from_model(User);
 
-    const new_user = {
+    const duplicate_user = {
       username: 'root',
       name: 'imposter superuser',
       password: 'password123'
@@ -55,13 +59,27 @@ describe('If there is one user in database', () => {
 
     const result = await api
       .post('/users/api')
-      .send(new_user)
+      .send(duplicate_user)
       .expect(400)
       .expect('Content-Type', /application\/json/);
     expect(result.body.error).toContain('This username is already taken');
 
     const altered_users = await get_all_from_model(User);
     expect(altered_users).toEqual(all_users);
+  });
+
+  test('fail to create a new user whose credentials are invalid', async () => {
+    const invalid_user = {
+      username: 'jo',
+      name: 'jo',
+      password: 'four'
+    };
+
+    await api
+      .post('/users/api')
+      .send(invalid_user)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
   });
 });
 
